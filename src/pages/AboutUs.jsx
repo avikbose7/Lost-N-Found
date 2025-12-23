7a4ffd30f0f5ca8d5dc1d11be9d6894502fd4fa3
Avik <avikbose916@gmail.com>
2025-12-23 22:48:03 +0530

    update

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Header from "@/components/Header";
 import LoginForm from "@/components/LoginForm";
 import Footer from  "@/components/Footer"
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 // Reverting to aliased paths
 import Header from "@/components/Header";
 import LoginForm from "@/components/LoginForm";
